4a98b1a2240007f91f5bd9a82926c94c46e5b29b
350254563 <350254563@C-MXL2393F1M.tdsb.on.ca>
2024-09-20 11:38:59 -0400

    Created all about me and my information

---
+++ ICS3U-ChesterG/src/unit1/AboutMe.java
@@ -0,0 +1,18 @@
+package unit1;
+/**
+ * Date: 09/20/24
+ * Description: This is all about me and my information (Name, Grade, School and Teacher)
+ * @author Gavin Chester
+ */
+public class AboutMe {
+
+	public static void main(String[] args) {
+		// This prints my information to the system console
+		System.out.println("Gavin Chester");
+		System.out.println("Grade 11");
+		System.out.println("Ms. Kemp");
+		System.out.println("Lawrence Park CI");
+
+	}
+
+}
