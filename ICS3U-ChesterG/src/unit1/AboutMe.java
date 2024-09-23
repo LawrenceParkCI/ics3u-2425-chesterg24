e085b8e7ee9478b374b6568bdf783469f495cc4d
350254563 <350254563@C-MXL2393F1M.tdsb.on.ca>
2024-09-23 09:45:00 -0400

    Added timetable for week of sept.23

--- ICS3U-ChesterG/src/unit1/AboutMe.java
+++ ICS3U-ChesterG/src/unit1/AboutMe.java
@@ -12,6 +12,22 @@ public class AboutMe {
 		System.out.println("Grade 11");
 		System.out.println("Ms. Kemp");
 		System.out.println("Lawrence Park CI");
+		System.out.println("GO PANTHERS!");
+		
+		System.out.println("\nMy Late Start Schedule:");
+		System.out.println("\nMonday (Day 1):");
+		System.out.println("\t 9:00-10:15 ICS3U1-1 Ms.Kemp");
+		System.out.println("\t 10:20-11:40 BAF3M1-3 Mr.Goldkind");
+		System.out.println("\t 11:40- 12:40 LUNCH!!!");
+		System.out.println("\t 12:40-1:55 NBE3U1-5 Mr. Solakis");
+		System.out.println("\t 2:00-3:15 BOH4M1-1 Mr.Goldkind");
+		
+		System.out.println("\nTuesday (Day 2):");
+		System.out.println("\t 9:00-10:15 BAF3M1-3 Mr.Goldkind");
+		System.out.println("\t 10:20-11:40 ICS3U1-1 Ms.Kemp");
+		System.out.println("\t 11:40- 12:40 LUNCH!!!");
+		System.out.println("\t 12:40-1:55 NBE3U1-5 Mr.Goldkind");
+		System.out.println("\t 2:00-3:15 BOH4M1-1 Mr. Solakis");
 
 	}
 
