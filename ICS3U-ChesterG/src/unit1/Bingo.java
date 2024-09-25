b9a297782dc5fa80c43e46145d5d57d91d96f6e0
350254563 <350254563@C-MXL2393F1M.tdsb.on.ca>
2024-09-25 10:56:51 -0400

    Finished Bingo Assignment

---
+++ ICS3U-ChesterG/src/unit1/Bingo.java
@@ -0,0 +1,29 @@
+package unit1;
+
+public class Bingo {
+
+	public static void main(String[] args) {
+		//This prints my information to the system console
+		System.out.println("BINGO CARD!!!");
+		System.out.println("\n");
+		System.out.println("\t1. The caller randomly pulls a numbered bingo ball.");
+		System.out.println("\n");
+		System.out.println("\t2. The number is placed on the bingo board and called out");
+		System.out.println("\n");
+		System.out.println("\t3. Players look for the called number on their bingo card");
+		System.out.println("\n");
+		System.out.println("\t4. If the number is located, it is marked off");
+		System.out.println("\n");
+		System.out.println("\t5. Steps 1 to 4 are repeated until a player matches the BINGO pattern.");
+		System.out.println("\n");
+		System.out.println("\t6. The winning player yells BINGO.");
+		System.out.println("\n");
+		System.out.println("\t B \t I \t N \t G \t O");
+		System.out.println("\t 2 \t 20 \t 42 \t 60 \t 64");
+		System.out.println("\t 14 \t 25 \t 32 \t 55 \t 70");
+		System.out.println("\t 5 \t 18 \t FREE \t 53 \t 67");
+		System.out.println("\t 12 \t 16 \t 31 \t 46 \t 75");
+		System.out.println("\t 10 \t 22 \t 39 \t 59 \t 71");
+	}
+
+}
