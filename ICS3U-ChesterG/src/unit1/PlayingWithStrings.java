7290d6f39eccf73b4b161f2458c58e5d600bc5a3
350254563 <350254563@C-MXL2393F1M.tdsb.on.ca>
2024-10-04 11:37:21 -0400

    Working on string worksheet

---
+++ ICS3U-ChesterG/src/unit1/PlayingWithStrings.java
@@ -0,0 +1,15 @@
+package unit1;
+
+public class PlayingWithStrings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String spaces = " Trim me ";
+		String noSpaces = spaces.trim();
+		System.out.println(spaces);
+		System.out.println(noSpaces);
+		
+
+	}
+
+}
