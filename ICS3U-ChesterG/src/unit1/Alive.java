4eacc7e07f45d38022c7189d8cb2a0efe9383667
350254563 <350254563@C-MXL2393F1M.tdsb.on.ca>
2024-10-07 10:12:51 -0400

    Working on ALive, changes on playing with strings

---
+++ ICS3U-ChesterG/src/unit1/Alive.java
@@ -0,0 +1,61 @@
+package unit1;
+
+import java.util.Scanner;
+
+public class Alive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//declare variables
+		
+		int birthdate, year, month, day, year1, month1, day1; 
+
+		
+		//get the user input 
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Alive");
+		System.out.print("Type in your birthdate: ");
+		System.out.print("\n");
+		
+		
+		System.out.print("Type in your birth year and <Enter>: ");
+		year = sc.nextInt();
+		
+		System.out.print("Type in your birth month and <Enter>: ");
+		month = sc.nextInt();
+		
+		System.out.print("Type in your birth date and <Enter>: ");
+		day = sc.nextInt();
+		
+		System.out.print("Type in today's date: ");
+		System.out.print("\n");
+	    
+		System.out.print("Type in this year and  <Enter>: ");
+		year1 = sc.nextInt();
+		
+		System.out.print("Type in this month and <Enter>: ");
+		month1 = sc.nextInt();
+		
+		System.out.print("Type in today's date and <Enter>: ");
+		day1 = sc.nextInt();
+		
+		
+		
+		//alive for how many days 
+		double total = ();
+		
+		
+		//print the output 
+		System.out.println("You have been alive for " + total + " days" );
+		sc.close();
+		
+				
+		
+		
+
+	}
+
+}
+//
