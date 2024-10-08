31ad4e728d91c368e64bae264baa69c384188378
350254563 <350254563@C-MXL2393F1M.tdsb.on.ca>
2024-10-08 10:42:53 -0400

    Completed the Alive program

--- ICS3U-ChesterG/src/unit1/Alive.java
+++ ICS3U-ChesterG/src/unit1/Alive.java
@@ -8,48 +8,52 @@ public class Alive {
 		// TODO Auto-generated method stub
 		//declare variables
 		
-		int birthdate, year, month, day, year1, month1, day1; 
+		int year1, month1, day1, year2, month2, day2, daysalive; 
 
 		
 		//get the user input 
 		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Alive");
-		System.out.print("Type in your birthdate: ");
+		System.out.println("Alive!");
+		System.out.print("Your birthdate: ");
 		System.out.print("\n");
-		
+		System.out.print("\n");
 		
 		System.out.print("Type in your birth year: ");
-		year = sc.nextInt();
-		
-		System.out.print("Type in your birth month: ");
-		month = sc.nextInt();
-		
-		System.out.print("Type in your birth date: ");
-		day = sc.nextInt();
-		
-		System.out.print("Type in today's date: ");
-		System.out.print("\n");
-	    
-		System.out.print("Type in this year: ");
 		year1 = sc.nextInt();
 		
-		System.out.print("Type in this month: ");
+		System.out.print("Type in your birth month: ");
 		month1 = sc.nextInt();
 		
-		System.out.print("Type in today's date: ");
+		System.out.print("Type in your birth date: ");
 		day1 = sc.nextInt();
+		
+		System.out.print("Today's date: ");
+		System.out.print("\n");
+		System.out.print("\n");
+	    
+		System.out.print("Type in this current year: ");
+		year2 = sc.nextInt();
+		
+		System.out.print("Type in this month number: ");
+		month2 = sc.nextInt();
+		
+		System.out.print("Type in today's date: ");
+		day2 = sc.nextInt();
+		System.out.print("\n");
+		
 		
 		
 		
 		//alive for how many days 
-		double total = ();
+	
 		
 		
 		//print the output 
-		System.out.println("You have been alive for " + total + " days" );
-		sc.close();
+		System.out.println("You have been alive for " + (daysalive = ((year2 - year1)*365)+ ((month2- month1)*30) + ((day2- day1))) + " days");
+		System.out.print("You have slept for " + (daysalive)*8 + " hours");
+		
 		
 				
 		
