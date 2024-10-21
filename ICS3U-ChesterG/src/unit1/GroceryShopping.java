56bb8d80e13903977011279971ec338592d14719
350254563 <350254563@C-MXL2393F1M.tdsb.on.ca>
2024-10-21 09:37:03 -0400

    Working on GroceryShopping

--- ICS3U-ChesterG/src/unit1/GroceryShopping.java
+++ ICS3U-ChesterG/src/unit1/GroceryShopping.java
@@ -1,19 +1,21 @@
 package unit1;
 
 import java.util.Scanner;
+import java.time.LocalDateTime; //LocalDateTime class
+import java.time.format.DateTimeFormatter; //DateTimeFormatter class
 
 public class GroceryShopping {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		Scanner sc = new Scanner(System.in);
 		System.out.printf("%5s%5s%5s%5s%5s%5s%5s%5s%5s", "$$$", "^^^", "*** ","|||||", "\"Mr. Chester's Grocery\"", "|||||","***", "^^^", "$$$");
 		
-		//declare variables
+		//Declare variables
 		
 		String product;
 		double cost, quantity;
+		LocalDateTime myDateObj = LocalDateTime.now();
 		
 		System.out.print("\n\n");
 		System.out.print("What product are you buying?");
@@ -24,8 +26,27 @@ public class GroceryShopping {
 		
 		System.out.print("How many are you buying?");
 		quantity = sc.nextDouble();
-		System.out.print("Very good.");
+		System.out.print("Very good."); 
 		
+		System.out.print("\n\n");
+		System.out.print("What product are you buying?");
+		product = sc.next();
+		
+		System.out.print("What is the cost of the product?");
+		cost = sc.nextDouble();
+		
+		System.out.print("How many are you buying?");
+		quantity = sc.nextDouble();
+		System.out.print("Very good\n\n"); 
+		
+		System.out.print("This is your recipt:\n\n"); 
+		System.out.print("\t Mr. Chester's Grocery\n"); 
+		DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+	    String formattedDate = myDateObj.format(myFormatObj);
+	    System.out.println(formattedDate);
+		 
 	}
 
 }
+
+
