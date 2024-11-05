ae50833644b26f6d2b3fede8e95294233c5e3237
350254563 <350254563@C-MXL2393F1M.tdsb.on.ca>
2024-11-05 10:16:01 -0500

    Completed work

--- ICS3U-ChesterG/src/unit2/IfStatements2.java
+++ ICS3U-ChesterG/src/unit2/IfStatements2.java
@@ -9,10 +9,31 @@ import java.util.Scanner;
 
 public class IfStatements2 {
   public static void main(String[] args) {
+	  
+	  
+	//User Input
     Scanner in = new Scanner(System.in);
-
     System.out.println("What is your age?");
+    
+    //Variables
     int userAge = in.nextInt();
+    
+    //Over 18
+    if (userAge > 18) { 
+        String title = "Adult";
+        System.out.println("title: "+ title);
+      } 
+    
+    //Not Over 18
+    else {  
+        System.out.println("Sorry, not quite yet.");
+      }
+
+
+
+    
+    
+    
     /*
     if (userAge > 18) { //If they're over 18, they are titled an adult
       String title = "Adult";
@@ -24,6 +45,7 @@ public class IfStatements2 {
     */
     //Looking at the logic above, why do you think the 
     //program won't work? 
+    //This program looks to be all jumbled up and not correct format.
 
     //Copy the code below, and try to find a solution.
     //Comment the code above to avoid compilation errors.
