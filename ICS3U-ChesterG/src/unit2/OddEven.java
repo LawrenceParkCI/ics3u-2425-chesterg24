970ca21d590c664d7886cb993bf4c7284c9f3fbe
350254563 <350254563@C-MXL2393F1M.tdsb.on.ca>
2024-11-05 09:20:48 -0500

    Completed BasicBoolean

--- ICS3U-ChesterG/src/unit2/OddEven.java
+++ ICS3U-ChesterG/src/unit2/OddEven.java
@@ -1,10 +1,19 @@
 package unit2;
+import java.util.Scanner;
 
 public class OddEven {
+ public static void main(String[] args) {
+     Scanner scanner = new Scanner(System.in);
+     System.out.print("Enter an integer: ");
+     int number = scanner.nextInt();
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+     // Check if the number is even or odd
+     if (number % 2 == 0) {
+         System.out.println("The number " + number + " is even.");
+     } else {
+         System.out.println("The number " + number + " is odd.");
+     }
 
-	}
-
+     scanner.close();
+ }
 }
--- ICS3U-ChesterG/src/unit2/PositiveNegative.java
+++ ICS3U-ChesterG/src/unit2/PositiveNegative.java
@@ -1,32 +1,29 @@
 package unit2;
-
 import java.util.Scanner;
 
 public class PositiveNegative {
+ public static void main(String[] args) {
+     Scanner scanner = new Scanner(System.in);
+     System.out.print("Enter an integer: ");
+     int number = scanner.nextInt();
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		//variables 
-		Scanner sc = new Scanner(System.in);
-		double integer;
-		
-		//input
-		System.out.print("Enter any number");
-		integer = sc.nextInt();
-		
-		//determine if the number is odd or even
-		if ((integer % 2) == 0) {
-			System.out.println("This number is even!");
-			   // even
-			} else {
-				System.out.println("This number is odd!");
-			   // odd
-			}
-		
-		
-		
-		
-	}
+     // Check if the number is positive or negative
+     if (number > 0) {
+         System.out.println("The number " + number + " is positive.");
+     } else if (number < 0) {
+         System.out.println("The number " + number + " is negative.");
+     } else {
+         System.out.println("The number is zero.");
+     }
 
+     // Check if the number is divisible by 7
+     if (number % 7 == 0) {
+         System.out.println("The number " + number + " is divisible by 7.");
+     } else {
+         System.out.println("The number " + number + " is not divisible by 7.");
+     }
+
+     scanner.close();
+ }
 }
+
