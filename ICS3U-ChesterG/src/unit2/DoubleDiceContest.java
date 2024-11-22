198d6acfba00b335a80fde5d944be44b9f722851
350254563 <350254563@C-MXL2393F1M.tdsb.on.ca>
2024-11-22 11:27:54 -0500

    Working on programs

--- ICS3U-ChesterG/src/unit2/DoubleDiceContest.java
+++ ICS3U-ChesterG/src/unit2/DoubleDiceContest.java
@@ -1,22 +1,50 @@
 package unit2;
-
-import java.util.Scanner;
-
 /**
- * Description: Double Dice Contest
- * Date: 2024/11/19
+ * Description: Two dice roll for user and computer 
+ * Date: 2024/11/22
  * @author: Gavin Chester
  */
 public class DoubleDiceContest {
 
- public static void main(String[] args) {
-  /*
-   * Write code that simulates rolling two dice for the user and then two dice for the computer. 
-   * The program should output the total of the two dice for both the user and the computer, 
-   * then announce who won: the computer, the user, or say it was a tie.
-   */
+/*
+ * Write code that simulates rolling two dice for the user and then two dice for the computer. 
+ * The program should output the total of the two dice for both the user and the computer, 
+ * then announce who won: the computer, the user, or say it was a tie.
+ */
 
- }
+	public static void main(String[] args) {
 
+
+		//Variables
+		int userdie1, userdie2, computerdie1, computerdie2;
+
+		//User dice roll
+		System.out.println("Roll a die");
+		userdie1 = (int)(Math.random()*6)+1;
+		System.out.println(userdie1);
+		System.out.println("Roll another die.");
+		userdie2 = (int)(Math.random()*6)+1;
+		System.out.println(userdie2);
+		System.out.println("Human score is " + (userdie1+userdie2));
+
+		//Computer dice roll
+		System.out.println("");
+		System.out.println("Computer roll");
+		computerdie1 = (int)(Math.random()*6)+1;
+		System.out.println(computerdie1);
+		System.out.println("Roll another die.");
+		computerdie2 = (int)(Math.random()*6)+1;
+		System.out.println(computerdie2);
+		System.out.println("Computer score is " + (computerdie1+computerdie2));
+
+		System.out.println(2);
+		if (userdie1+userdie2<computerdie1+computerdie2) {
+		System.out.println("You win!"); 
+	}
+	else {
+	System.out.println("Computer Wins!");
+	}
 }
+}
+
 
--- ICS3U-ChesterG/src/unit2/While.java
+++ ICS3U-ChesterG/src/unit2/While.java
@@ -1,44 +1,59 @@
 package unit2;
-	/**
-	 Description: While
-	 Date: 2024/11/21
-	 @author: Gavin Chester  
-	*/
+/**
+ * Description: While program
+ * Date: 2024/11/22
+ * @author: Gavin Chester
+ */
+import java.util.Scanner;
 
-	import java.util.Scanner;
+public class While {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("How many people are you planning to see?");
 
-	public class While {
-	  public static void main(String[] args) {
-	    Scanner in = new Scanner(System.in);
-	    System.out.println("How many people are you planning to see?");
+		//Why do you think I coded it this way?
+		int numPeople = Integer.parseInt(in.nextLine());
 
-	    //Why do you think I coded it this way? -return an integer number as input
-	    int numPeople = Integer.parseInt(in.nextLine());
+		while (numPeople > 0) {
+			System.out.print("What is this person's name? ");
 
-	    while (numPeople > 0) {
-	      System.out.print("What is this person's name? ");
+			//Why do you think the variable is declared here?
+			String name = in.nextLine();
+			System.out.println("Welcome, " + name + "!");
 
-	      //Why do you think the variable is declared here?
-	      String name = in.nextLine();
-	      System.out.println("Welcome, " + name + "!");
-
-	      numPeople = numPeople - 1;
-	    }
-	    
-
-	    in.close();
-	    //Run the code Using the debugger. 
-	    //Explain the code in your own words
-
-	    //What code is repeated?
-	    //When does it decide whether to repeat or not?
-	    //What is the condition for repeating?
+			numPeople = numPeople - 1;
+		}
 
 
-	    //What is the difference between a while loop and a do-while loop?
+		in.close();
+		//Run the code Using the debugger. 
+		//Explain the code in your own words
+		//This code asks for a number of people and then asks for each person's name.
+
+		//What code is repeated?
+		//"What is this person's name?" and "Welcome,..!" code are repeated
+
+		//When does it decide whether to repeat or not?
+		//"When there are same number of names as people entered."
+
+		//What is the condition for repeating?
+		//This condition is for repeating: "What is this persons name?" until the number of names as people is entered.
 
 
-	    //Create a while loop that asks for a word, 
-	    //and prints it out 20 times.
-	  }
+		//What is the difference between a while loop and a do-while loop?
+		//The do-while loop is used when the number of iterations is uncertain, where the while loop is typically used when you want to repeat a task a specific number of times.
+
+
+		//Create a while loop that asks for a word, 
+		//and prints it out 20 times.
+
+		System.out.print("Type any word.");
+		String word = in.nextLine();
+		int repeat = 1;
+		while (repeat<=20 ) {
+			System.out.println(word);
+			repeat ++;
+		}
+		in.close();
 	}
+}
