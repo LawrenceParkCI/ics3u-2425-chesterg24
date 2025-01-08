65bda2f3cb6abd652df07e521daadaa4f86632ea
350254563 <350254563@C-MXL2393F1M.tdsb.on.ca>
2025-01-08 11:54:15 -0500

    Setting up my culminating project as I was not here yesterday because of school ski team

---
+++ ICS3U-ChesterG/src/culminating2025/TicTacToe.java
@@ -0,0 +1,16 @@
+package culminating2025;
+/*
+ * Name: Gavin Chester
+ * Date: 2025/01/08
+ * Description:
+ */
+import java.awt.*;
+import java.io.Console;
+public class TicTacToe {
+	static Console c;
+	public static void main(String[] args) {
+		
+
+	}
+
+}
